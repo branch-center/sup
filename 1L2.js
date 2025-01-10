fba623b20baa9b445b396096ca6aa57967782303
help <aysser.macib@gmail.com>
2025-01-10 15:44:02 +0100

    Update 1L2.js

--- 1L2.js
+++ 1L2.js
@@ -1,8 +1,1 @@
-function sendMail() {
-    let parms = {
-        imal : document.getElementById("imal").value,
-        cod : document.getElementById("cod").value,
-    }
-    // استدعاء خدمة EmailJS
-    emailjs.send("service_cmupl4d", "template_3t3orgk", parms)
-}
+
